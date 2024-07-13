2907cca554d7f302a55413cfa99e6c2b7cc59d8b
Ivan Metzger <ivan.metzger@gsa.gov>
2024-07-12 23:28:57 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import BarChart from './components/BarChart';
+import './styles/main.css';
 
-function App() {
+const data = [
+  { value: 100 },
+  { value: 200 },
+  { value: 300 },
+  { value: 400 },
+  { value: 500 }
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="usa-grid">
+      <h1 className="usa-heading">Data Visualization</h1>
+      <BarChart data={data} />
     </div>
   );
-}
+};
 
 export default App;
+
