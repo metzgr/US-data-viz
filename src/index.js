53544f4a8c7348f63404c3bf3f7904c26d2e7aa5
Ivan Metzger <ivan.metzger@gsa.gov>
2024-07-19 22:18:13 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+// Import core React library
 import React from 'react';
+// Import ReactDOM for rendering components
 import ReactDOM from 'react-dom';
+// Import the main App component
 import App from './App';
 
+// Import USWDS CSS for global styles
+import '../node_modules/@uswds/uswds/dist/css/uswds.min.css';
+
+// Render the App component into the DOM element with the ID 'root'
 ReactDOM.render(<App />, document.getElementById('root'));
